0b3718e1666a2671c53f31802b76339ee239b8d7
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-02 12:53:21 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
+import hidden_4 as hidden
 if __name__ != "__main__":
     exit()
-    import hidden_4
-    for name in sorted (dir(hidden_4)):
-        if name[:2] != '__':
-            print("{}".format(name))
+for name in dir(hidden):
+    if name[0:2] != "__":
+	    print(name)
