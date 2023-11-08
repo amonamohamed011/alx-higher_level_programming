6b72b6048faabf5b041deb21e5722efa8b474816
amonamohamed011 <amonamohamed011@gmail.com>
2023-11-08 07:58:49 -0800

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     return ste_1 & set_2
+# The intersection set of A and B, denoted "A n B" (read "A inter B")
+# is the set of elements of A which are also elements of B
+# we can use set_1.intersection(set_2) too
